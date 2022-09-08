be2ad9d8a27f27be6cdaf33fe858457addb9c238
Masbahul-bari <okilul3g@gmail.com>
2022-09-08 21:19:23 +0600

    main

---
+++ main.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>main</title>
+	<style>
+		h1 {text-align: center;}
+		div.container {
+                      text-align: center;
+                    }
+
+             ul.myUL {
+                     display: inline-block;
+                     text-align: left;
+                    }
+	</style>
+</head>
+<body>
+	
+     <h1><u><font size="+7">Please Select an option.</font></u></h1>
+	<div class="container">
+	<ul class="myUL">
+		<li><font size="+5"><a href="create.php" target="_blank" style="text-decoration: none;">Creat Account</a></font></li>
+
+		<li><font size="+5"><a href="viewTotalAmount.php" target="_blank" style="text-decoration: none;">View total amount</a></font></li>
+
+		<li><font size="+5"><a href="delete.php" target="_blank" style="text-decoration: none;">Delete an account</a></font></li>
+
+		<li><font size="+5"><a href="viewAllAccount_db.php" target="_blank" style="text-decoration: none;">View all the account</a></font></li>
+
+		<li><font size="+5"><a href="updateAccount.php" target="_blank" style="text-decoration: none;">Update an account</a></font></li>
+
+		<li><font size="+5"><a href="searchAccount.php" target="_blank" style="text-decoration: none;">Search an account</a></font></li>	
+	</ul>
+	</div>
+
+</body>
+</html>
