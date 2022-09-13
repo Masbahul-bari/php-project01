566e04a2342a0a881ee7ab15d9d521a2d294ec29
Masbahul-bari <okilul3g@gmail.com>
2022-09-13 22:03:40 +0600

    delete

---
+++ delete.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Delete an account</title>
+	<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+	<section class="booking">
+
+   <h1 class="heading-title">Delete Account</h1>
+
+   <form action="d_db.php" method="post" class="book-form">
+
+      <div class="flex">
+         <div class="inputBox">
+            <span>ID :</span>
+            <input type="number" placeholder="Enter your ID" name="id">
+         </div>
+
+      <input type="submit" value="submit" class="btn" name="send">
+
+   </form>
+
+</section>
+
+</body>
+</html>
